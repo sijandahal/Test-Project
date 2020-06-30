4415b53bb9b35cef1c4faa98d5ad3cf29f814070
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2020-06-30 18:19:32 +0545

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector ('.nav__items');
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__menu')
+
+hamburger.addEventListener('click', function(){
+  
+  hamburger.classList.toggle('open');
+  overlay.classList.toggle('display');
+  menu.classList.toggle ('active')
+})
